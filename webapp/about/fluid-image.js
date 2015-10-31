ce165594574b1bd2de603aa6a1247b25ffbf86cf
Taylor Edwards <9tedwards@gmail.com>
2015-10-31 12:08:13 -0700

    Tidied HTML, reset typography to Ionic styles for subheads (h4-h6) and body copy with Yodel-style type on primary headers (h1-h3).\nAdded .container class to manage maximum content width – no available utility from Ionic.\nRemoved scripts which changed the markup based on screen size – views should be handled with CSS alone.\nAdded styles to about.styl to correctly display the about page.\nTODO: Style the 'Contact Us' section.

--- webapp/about/fluid-image.js
+++ webapp/about/fluid-image.js
@@ -4,17 +4,7 @@ function FluidImage($window, $log) {
     transclude: true,
     scope: {
       src: '@'
-    },
-    controller: ['$scope', ($scope) => {
-      $log.info($scope.src);
-      var updateStyles = () => {
-        $scope.imageWidth = $window.innerWidth;
-        $scope.imageHeight = ((463 / 960) * $window.innerWidth);
-        $log.info('resizing image to ' + $scope.imageWidth + ' x ' + $scope.imageHeight);
-      };
-      updateStyles();
-      $scope.$watch(() => $window.innerWidth, updateStyles);
-    }]
+    }
   };
 }
 
